86dc58113750414ab4a7816e32838c12b1135843
FrauSchinkelmani <121432397+FrauSchinkelmani@users.noreply.github.com>
2023-01-22 11:47:52 +0100

    On button A pressed german countdown

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
-for (let index = 0; index <= 4; index++) {
-    music.playTone(262, music.beat(BeatFraction.Quarter))
-    basic.showNumber(5 - index)
-}
-music.playTone(392, music.beat(BeatFraction.Whole))
-basic.showString("LOS")
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index <= 4; index++) {
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        basic.showNumber(5 - index)
+    }
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    basic.showString("LOS")
+})
 basic.forever(function () {
 	
 })
